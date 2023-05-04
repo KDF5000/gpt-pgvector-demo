c4100a372d23f06109066402538d129466a9efbc
kongdefei <kongdefei@bytedance.com>
2023-05-05 00:12:32 +0800

    comment

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,47 +1,47 @@
 import { Index, Show, createEffect, createSignal, onCleanup, onMount } from 'solid-js'
 import { useThrottleFn } from 'solidjs-use'
-import GPT3Tokenizer from 'gpt3-tokenizer'
+// import GPT3Tokenizer from 'gpt3-tokenizer'
 import IconClear from './icons/Clear'
 import MessageItem from './MessageItem'
 import ErrorMessageItem from './ErrorMessageItem'
 import type { ChatMessage, ErrorMessage } from '@/types'
 
-const systemContent = `You are a helpful assistant. When given CONTEXT you answer questions using only that information,
-and you always format your output in markdown. You include code snippets if relevant. If you are unsure and the answer
-is not explicitly written in the CONTEXT provided, you say
-"Sorry, I don't know how to help with that."  If the CONTEXT includes 
-source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls 
-from the CONTEXT, but never list a URL more than once (ignore trailing forward slashes when comparing for uniqueness). Never include URLs that are not in the CONTEXT sections. Never make up URLs`
+// const systemContent = `You are a helpful assistant. When given CONTEXT you answer questions using only that information,
+// and you always format your output in markdown. You include code snippets if relevant. If you are unsure and the answer
+// is not explicitly written in the CONTEXT provided, you say
+// "Sorry, I don't know how to help with that."  If the CONTEXT includes
+// source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls
+// from the CONTEXT, but never list a URL more than once (ignore trailing forward slashes when comparing for uniqueness). Never include URLs that are not in the CONTEXT sections. Never make up URLs`
 
-const userContent = `CONTEXT:
-Next.js is a React framework for creating production-ready web applications. It provides a variety of methods for fetching data, a built-in router, and a Next.js Compiler for transforming and minifying JavaScript code. It also includes a built-in Image Component and Automatic Image Optimization for resizing, optimizing, and serving images in modern formats.
-SOURCE: nextjs.org/docs/faq
-QUESTION: 
-what is nextjs?    
-`
+// const userContent = `CONTEXT:
+// Next.js is a React framework for creating production-ready web applications. It provides a variety of methods for fetching data, a built-in router, and a Next.js Compiler for transforming and minifying JavaScript code. It also includes a built-in Image Component and Automatic Image Optimization for resizing, optimizing, and serving images in modern formats.
+// SOURCE: nextjs.org/docs/faq
+// QUESTION:
+// what is nextjs?
+// `
 
-const assistantContent = `Next.js is a framework for building production-ready web applications using React. It offers various data fetching options, comes equipped with an integrated router, and features a Next.js compiler for transforming and minifying JavaScript. Additionally, it has an inbuilt Image Component and Automatic Image Optimization that helps resize, optimize, and deliver images in modern formats.
-\`\`\`js
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
-export default HomePage
-\`\`\`
-SOURCES:
-https://nextjs.org/docs/faq`
+// const assistantContent = `Next.js is a framework for building production-ready web applications using React. It offers various data fetching options, comes equipped with an integrated router, and features a Next.js compiler for transforming and minifying JavaScript. Additionally, it has an inbuilt Image Component and Automatic Image Optimization that helps resize, optimize, and deliver images in modern formats.
+// \`\`\`js
+// function HomePage() {
+//   return <div>Welcome to Next.js!</div>
+// }
+// export default HomePage
+// \`\`\`
+// SOURCES:
+// https://nextjs.org/docs/faq`
 
 const promptMessages: ChatMessage[] = [
   {
     role: 'system',
-    content: systemContent,
+    content: 'systemContent',
   },
   {
     role: 'user',
-    content: userContent,
+    content: 'userContent',
   },
   {
     role: 'assistant',
-    content: assistantContent,
+    content: 'assistantContent',
   },
 ]
 
